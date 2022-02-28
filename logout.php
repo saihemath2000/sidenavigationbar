05537a3c07d3ebd99e8a8bf402550fc051000b8e
saihemath2000 <saihemath2000@gmail.com>
2022-02-28 20:10:10 +0530

    commit

--- logout.php
+++ logout.php
@@ -2,5 +2,5 @@
 include('../registration/functions.php');
 session_destroy();
 unset($_SESSION['user']);
-header("location: ../registration/signup_and_signin_folder/signin.php");
+header("location: ../teacherregistration/teacher_login.php");
 ?>
