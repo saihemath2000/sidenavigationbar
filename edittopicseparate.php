7bc84e1a27438be7ecb7cef5cff42abc47bbfd46
Hemanth <saihemath2000@gmail.com>
2022-03-27 12:11:56 +0530

    commit

--- edittopicseparate.php
+++ edittopicseparate.php
@@ -31,6 +31,7 @@ else{
       integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
       crossorigin="anonymous"
     />
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css'>    
     <style>
         body{
             font-size:20px;
@@ -43,20 +44,55 @@ else{
    $title = $_POST['topictitle'];
    $topicvideo = $_FILES['topicvideo']['name'];
    $tmp_topicvideo = $_FILES['topicvideo']['tmp_name'];
-   $totalfiles = count($_FILES['file']['name']);
+  //  $totalfiles = count($_FILES['file']['name']);
+   $files = array();
+   $files = $_FILES['files0'];
+      foreach($_FILES['files0']['name'] as $m=>$n){
+        if (move_uploaded_file($_FILES['files0']['tmp_name'][$m],'topicdocuments/' . $n)) {
+            //  echo 'uploaded';
+        } 
+      }
+      $browserIterator = 1;
+      while(isset($_FILES['files'.$browserIterator])) {
+          //Files have same attribute structure, so grab each attribute and append data for each attribute from each file
+          foreach($_FILES['files'.$browserIterator] as $attr => $values) {//get each attribute
+            foreach($_FILES['files'.$browserIterator][$attr] as $fileValue){//get each value from attribute
+                $files[$attr][] = $fileValue;//append value
+            }
+        }
+        foreach($_FILES['files'.$browserIterator]['name'] as $m=>$n){
+            if (move_uploaded_file($_FILES['files'.$browserIterator]['tmp_name'][$m],'topicdocuments/' . $n)) {
+                //  echo $browserIterator;
+            } 
+        }
+        $browserIterator++;
+    }
+    //Use $files like you would use $_FILES['browser'] -- It is as though all files came from one browser button!
+    $fileIterator = 0;
+    $string=$result1[5];
+    // echo count($files['name']);
+    while($fileIterator < count($files['name'])) {
+      if(strpos($string,$files['name'][$fileIterator]) !== false){
+          //  echo "Word Found!";
+          $fileIterator++;
+      } else{
+        $string = $string.':'.$files['name'][$fileIterator];
+        $fileIterator++;
+      }
+    }
    if($topicvideo){}
    else{
        $topicvideo= $result1[4];
    }
-   for ($i = 0; $i < $totalfiles; $i++) {
-    $filename = $_FILES['file']['name'][$i];
-    $string=$string.':'.$filename; 
-    if (move_uploaded_file($_FILES["file"]["tmp_name"][$i], 'topicdocuments/' . $filename)) {
-        //echo 'uploaded;
-    }
-   } 
-   $k =$result1[5];
-   $temp =$k.':'.$string;  
+  //  for ($i = 0; $i < $totalfiles; $i++) {
+  //   $filename = $_FILES['file']['name'][$i];
+  //   $string=$string.':'.$filename; 
+  //   if (move_uploaded_file($_FILES["file"]["tmp_name"][$i], 'topicdocuments/' . $filename)) {
+  //       //echo 'uploaded;
+  //   }
+  //  } 
+  //  $k =$result1[5];
+  //  $temp =$k.':'.$string;  
    if (isset($topicvideo)) {
         if (move_uploaded_file($tmp_topicvideo, './topicvideos/' . $topicvideo)) {
         // echo 'uploaded';
@@ -64,7 +100,7 @@ else{
     } else {
          echo 'f**k';
     }
-   $edit = mysqli_query($db,"UPDATE topic set title='$title',video='$topicvideo',document='$temp' where coursename='$course' and sectionname='$module' and title='$topic'");
+   $edit = mysqli_query($db,"UPDATE topic set title='$title',video='$topicvideo',document='$string' where coursename='$course' and sectionname='$module' and title='$topic'");
    if($edit){ 
        mysqli_close($db);
        header("location:edittopic.php?course=$course& module=$module"); 
@@ -114,12 +150,15 @@ else{
               <input
                 type="file"
                 class="form-control-file"
-                id="exampleFormControlFile1"
-                name="file[]"
+                id="files0"
+                name="files0[]"
                 multiple
               />
             </div>
-            <a href="" style="text-decoration:none;font-size:18px;margin-left:20px;">Click to add Quiz</a>
+            <span style="font-size: 10pt;margin-left:20px;">Click "+" for more files
+            <a><i id="more_files" class='fa fa-plus'></i></a></span>
+            <br><br>
+            <a href="" style="text-decoration:none;font-size:18px;">Click to add Quiz</a>
             <center><button
               type="submit"
               name="submittopic"
@@ -133,5 +172,23 @@ else{
       </div>
     </div>
   </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js" ></script>
+    <script type="text/javascript">   
+    $(document).ready(function() {
+        $(document).on('click','#more_files', function() {
+            var numOfInputs = 1;
+            while($('#files'+numOfInputs).length) { numOfInputs++; }//once this loop breaks, numOfInputs is greater than the # of browse buttons
+    
+            $("<input type='file' multiple/>")
+                .attr("style","font-size:20px;")
+                .attr("id", "files"+numOfInputs)
+                .attr("name", "files"+numOfInputs+"[]")
+                .insertAfter("#files"+(numOfInputs-1));
+    
+            $("<br/>").insertBefore("#files"+numOfInputs);
+            $('#files'+(numOfInputs-1)).hide();
+        });
+    });
+    </script>
 </body>
 </html>
